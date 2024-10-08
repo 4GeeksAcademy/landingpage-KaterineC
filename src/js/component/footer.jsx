0b5716170aabd7f373c322bd35034b09cf5bf642
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-10-08 21:22:56 +0000

    avance en bootstrap

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer className="m-0">
+    <p className="m-0 bg-dark p-5 text-white text-center text-body-secondary">Copyright © Your Website 2019</p>
+  </footer>
+
+    );
+
+}
+
+export default Footer
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+const NavBar = () => {
+return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Second navbar example">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample02">
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+}
+
+export default NavBar
